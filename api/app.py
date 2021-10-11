57eb4ae7eadbeddf493eb95d598e34e00f17979c
petermaksymo <peter.maksymowsky@mail.utoronto.ca>
2021-10-11 14:53:47 -0400

    Added postgres docker container

--- api/app.py
+++ api/app.py
@@ -1,8 +1,12 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super secret string that will never be cracked or pushed to a public github repo'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
