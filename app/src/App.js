3c9863b37f79ce367880d811bb74ccb3b3c5704e
Yousif Al-Furaiji <yousif.alfuraiji@gmail.com>
2021-10-12 00:44:44 -0400

    added commented code for basic communication between frontend and backend

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import { NavBar } from './Components/navbar';
+import React from 'react';
 
 function App() {
+
+  //   Basic code showing how to communicate between our backend and frontend
+  //   useEffect( () => {
+  //     fetch('/').then(response => {
+  //         if(response.ok) {
+  //             return response.json()
+  //         }
+  //     }).then(data => console.log(data))
+  // })
+
+
   return (
     <div className="App">
       <NavBar></NavBar>
