92c8c0e8c52cee6c21d89e8a33fe7126a58a2be0
Hoowolf <43775437+Hoowolf@users.noreply.github.com>
2021-10-24 20:11:20 -0400

    constructed table for courses information

--- api/app.py
+++ api/app.py
@@ -1,6 +1,8 @@
 from flask import jsonify
 
-from api import create_app
+from api import create_app, database
+
+from api.database import db
 
 app = create_app('development')
 
@@ -8,5 +10,12 @@ app = create_app('development')
 def index():
     return jsonify(status='API is Up!')
 
+@app.route('/recreatedatabase')
+def recreatedatabase():
+    return '<h1> Database Re-created'
+    
 # Import the account related endpoints
 import api.routes.account
+
+# Import the course related endpoints
+import api.routes.course
