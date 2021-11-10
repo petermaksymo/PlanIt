bddd26ec16e107e7efe3679f7e69692206d2cc28
Alan Du <torterra1999@hotmail.com>
2021-11-10 13:27:43 -0500

    created schema for profile

---
+++ api/database/models/profile.py
@@ -0,0 +1,17 @@
+from api import db
+from api.database import Serializer
+
+class Profile(db.Model):
+    """Model for profiles"""
+
+    __tablename__ = 'profile'
+
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    account_id = db.Column(db.Integer, unique=False, nullable=False)
+    profile_name = db.Column(db.String, unique=False, nullable=False)
+    session_name = db.Column(db.String, unique=False, nullable=True)
+    course_name = db.Column(db.String, unique=False, nullable=True)
+
+def serialize(self):
+    d = Serializer.serialize(self)
+    return d
