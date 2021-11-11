a83d62e9f2fdb16eb92bb2e66cb94df7ebf2fff8
Hoowolf <43775437+Hoowolf@users.noreply.github.com>
2021-11-10 19:59:23 -0500

    complete login logout signup account_information methods

--- api/routes/account.py
+++ api/routes/account.py
@@ -6,18 +6,12 @@ from werkzeug.utils import html
 from api.app import app
 from api.database import db
 from api.database.models import Account
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 
-@app.route('/account', methods=['GET', 'POST'])
+""" @app.route('/account', methods=['GET', 'POST', 'PATCH', 'DELETE'])
 def account():
     if request.method == 'POST':
-        name = request.form['name']
-        password = request.form['password']
-        new_account = Account(name=name, password=password, created_at=datetime.now())
-        db.session.add(new_account)  # Adds new User record to database
-        db.session.commit()  # Commits all changes
-        return jsonify(new_account.serialize())
-
+        signup_post()
     elif request.method == 'GET':
         name = request.args.get('name', '')
         account = Account.query.filter_by(name=name).first()
@@ -25,7 +19,36 @@ def account():
         if not account:
             return jsonify({ 'error': 'account does not exist' }), 404
 
-        return jsonify(account.serialize())
+        return jsonify(account.serialize()) """
+
+@app.route('/account', methods=['GET', 'PATCH', 'DELETE'])
+@login_required
+def account():
+    if request.method == 'GET':
+        return jsonify(current_user.serialize())
+    elif request.method == 'PATCH':
+        email = request.form.get('email')
+        name = request.form.get('name')
+        password = request.form.get('password')
+        new_account = Account.query.filter_by(id=current_user.id).first()
+        if (email):
+            new_account.email = email
+        elif (name):
+            new_account.name = name
+        elif (password):
+            new_account.password = generate_password_hash(password, method='sha256')
+        db.session.add(new_account)
+        db.session.commit()
+        return '<h1>complete update</h1>'
+    elif request.method == 'DELETE':
+        deleting_account = Account.query.filter_by(id=current_user.id).first()
+        db.session.delete(deleting_account)
+        db.session.commit()
+        return '<h1>complete delete</h1>'
+    else:
+        raise Exception("Unsupported Request!")
+
+        
 
 @app.route('/signup')
 def signup():
@@ -40,7 +63,6 @@ def signup_post():
     if (user):
         flash('Email address already exists')
         return redirect(url_for('signup'))
-
     new_user = Account(email=email, name=name, password=generate_password_hash(password, method='sha256'), created_at= datetime.datetime.now()) 
     db.session.add(new_user)
     db.session.commit()
@@ -54,9 +76,7 @@ def login():
 def login_post():
     email = request.form.get('email')
     password = request.form.get('password')
-
     user = Account.query.filter_by(email=email).first()
-
     if not user or not check_password_hash(user.password, password):
         flash('Please check your login details and try again.')
         return "<h1>wrong password or wrong email</h1>"
