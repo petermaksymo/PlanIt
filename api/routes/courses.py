2e1ae8959c570b4ce5dbc4002d93a6d519bafdf4
Hoowolf <43775437+Hoowolf@users.noreply.github.com>
2021-11-14 23:41:38 -0500

    refactor /course/<code> api

--- api/routes/courses.py
+++ api/routes/courses.py
@@ -54,49 +54,11 @@ def search_courses(search_keywords, year, divisions, departments, campuses, top)
 
 """
 This method shows the information about a single course.
-First, some basic error handling for if a course is passed that does not exist.
-Then, separate the course information into the elements which have specific display functionality and the rest, which we show in a big table.
-Pass all that to render template.
 """
 @app.route("/course/<code>")
 def course(code):
+    query = db.session.query(Course)
+    course = query.filter_by(code=code).first()
+    return jsonify(course.serialize())
 
-    # If the course code is not present in the dataset, progressively remove the last character until we get a match.
-    # For example, if there is no CSC413 then we find the first match that is CSC41.
-    # If there are no matches for any character, just go home.
-    if code not in df.index:
-        while True:
-            code = code[:-1]
-            if len(code) == 0:
-                return redirect("/")
-            t = df[df.index.str.contains(code)]
-            if len(t) > 0:
-                code = t.index[0]
-                return redirect("/course/" + code)
-
-    course = df.loc[code]
-    # use course network graph to identify pre and post requisites
-    course = {
-        k: v
-        for k, v in course.items()
-        if k
-        not in [
-            "Course",
-            "Course Level Number",
-            "FASEAvailable",
-            "MaybeRestricted",
-            "URL",
-            "Pre-requisites",
-            "Exclusion",
-            "Corequisite",
-            "Recommended Preparation",
-            "AIPreReqs",
-            "MajorsOutcomes",
-            "MinorsOutcomes",
-            "Term",
-            "Activity",
-        ]
-        and v == v
-    }
-    return jsonify(course)
 
