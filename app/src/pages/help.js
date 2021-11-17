bab52bd4ecb791de9184adebe73245522602b182
petermaksymo <peter.maksymowsky@mail.utoronto.ca>
2021-11-17 01:11:27 -0500

    Made help page responsive

--- app/src/pages/help.js
+++ app/src/pages/help.js
@@ -27,35 +27,69 @@ export const Help = () => {
       <div
         id="profiles"
         style={{
-          padding: "30px 286px",
+          padding: "30px 0",
           backgroundColor: theme.palette.background.lightPink,
         }}
       >
-        <Typography
+        <div
           style={{
-            color: theme.palette.text.dark,
-            fontSize: 28,
-            marginBottom: "15px",
-            fontWeight: "bold",
+            maxWidth: 1440,
+            margin: "auto",
+            padding: "0 24px",
+            boxSizing: "border-box",
           }}
         >
-          Help
-        </Typography>
+          <Typography
+            style={{
+              color: theme.palette.text.dark,
+              fontSize: 28,
+              marginBottom: "15px",
+              fontWeight: "bold",
+            }}
+          >
+            Help
+          </Typography>
+        </div>
       </div>
-      <div id="page-container" style={{ margin: "80px 309px" }}>
-        <Typography className={classes.text}>
-          The following video is a tutorial on how to use most of the websites
-          features
-        </Typography>
-        <iframe
-          src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-          frameborder="0"
-          width="853"
-          height="480"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-          title="Help Video"
-        />
+      <div id="page-container" style={{ margin: "40px 0" }}>
+        <div
+          style={{
+            maxWidth: 1440,
+            margin: "auto",
+            padding: "0 24px",
+            boxSizing: "border-box",
+          }}
+        >
+          <Typography className={classes.text}>
+            The following video is a tutorial on how to use most of the websites
+            features
+          </Typography>
+          <div
+            style={{
+              position: "relative",
+              overflow: "hidden",
+              width: "100%",
+              paddingTop: "56.25%",
+            }}
+          >
+            <iframe
+              style={{
+                position: "absolute",
+                width: "100%",
+                height: "100%",
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+              }}
+              src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+              title="Help Video"
+            />
+          </div>
+        </div>
       </div>
     </>
   )
